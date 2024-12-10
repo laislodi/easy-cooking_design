9c3ce3013f83787518e1176f433c2ab7dbf6fdb8
Lais Gabrielle Lodi <gabriellelodi@gmail.com>
2024-12-09 17:41:47 -0700

    Add backdrop and modal
    - Add mobile menu
    - Add js to control mobile menu
    - Move CSS for menu from main to common

--- shared.js
+++ shared.js
@@ -1,32 +1,45 @@
 var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector('.modal');
 
+// console.dir(backdrop.style['background-color']);
+// console.dir(backdrop.style.backgroundColor);
+
 var selectPlanButtons = document.querySelectorAll(".plan button");
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function () {
-        backdrop.style.display = 'block';
-        modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        backdrop.classList.add('open');
+        modal.classList.add('open');
     });
 }
 
 var noButton = document.querySelector('.modal-action__negative');
 noButton.addEventListener('click', function() {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
 });
 
 var mobileNav = document.querySelector('.mobile-nav');
 backdrop.addEventListener('click', function() {
-    mobileNav.style.display = 'none';
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    // mobileNav.style.display = 'none';
+    // modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    mobileNav.classList.remove('open');
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
+    
 });
 
 var toggleButton = document.querySelector('.toggle-button');
 toggleButton.addEventListener('click', openMobileNav)
 
 function openMobileNav() {
-    mobileNav.style.display = 'block';
-    backdrop.style.display = 'block';
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
 }
 
